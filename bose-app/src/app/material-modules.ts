bc1ee1be63eb7aa159a4667b9416819a8c682703
shengjlim <sheng-jie.lim@huskers.unl.edu>
2019-10-25 22:18:11 -0400

    Commit with the basic angular app and angular material built in

---
+++ bose-app/src/app/material-modules.ts
@@ -0,0 +1,10 @@
+import { MatButtonModule } from '@angular/material/button';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    exports: [
+        MatButtonModule
+    ]
+})
+
+export class MaterialModule { };
