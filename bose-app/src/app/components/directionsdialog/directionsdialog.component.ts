15d21ba8003d3b749be180805993f4e22fe5d0ee
shengjlim <sheng-jie.lim@huskers.unl.edu>
2019-10-27 02:06:34 -0400

    Final styling

--- bose-app/src/app/components/directionsdialog/directionsdialog.component.ts
+++ bose-app/src/app/components/directionsdialog/directionsdialog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
 
 @Component({
   selector: 'app-directionsdialog',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectionsdialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<DirectionsdialogComponent>,
+  ) { }
 
   ngOnInit() {
+  }
+  close(): void {
+    this.dialogRef.close();
   }
 
 }
